c8adaf7ff9e8b56e0d31a7d4fb046bc41d9184fe
Kishan S Patel <bishan.batel@protonmail.com>
2024-02-07 05:20:57 -0800

    Cargo

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod parser;
+
+use std::path::{PathBuf};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+	path: PathBuf,
+}
+
+#[derive(Debug, thiserror::Error)]
+enum ArgumentError {
+	#[error("Could not find file to read")]
+	FileNotFound,
+
+	#[error("Could not open file to read")]
+	CouldNotOpenFile,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let args = Args::parse();
+
+	if !args.path.exists() {
+		Err(ArgumentError::FileNotFound)?;
+	}
+
+	println!("Parsing file {:#?}", args.path.as_path());
+
+	// we have a tree_entered virtual method u can override, as well as a on_tree_entered signal
+
+	if let Ok(contents) = std::fs::read_to_string(args.path) {
+		parser::parse(contents)?;
+	} else {
+		Err(ArgumentError::CouldNotOpenFile)?;
+	}
+
+	Ok(())
+}
